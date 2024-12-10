935f1e836af7a909f20e0abf60044c98c90ea5c6
Antoine <a.fourcade65@gmail.com>
2024-12-10 17:56:28 +0100

    WIP jour 10 partie 1

---
+++ 2024/day10/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const (
+	zero trailChar = '0'
+	one trailChar = '1'
+	two trailChar = '2'
+	three trailChar = '3'
+	four trailChar = '4'
+	five trailChar = '5'
+	six trailChar = '6'
+	seven trailChar = '7'
+	eight trailChar = '8'
+	nine trailChar = '9'
+)
+
+type trailChar rune
+
+type Trails [][]trailChar
+
+var nextChar = map[trailChar]trailChar{
+	one: two,
+	two: three,
+	three: four,
+	four: five,
+	five: six,
+	six: seven,
+	seven: eight,
+	eight: nine,
+}
+
+func (t Trails) String() string {
+	var out string
+
+	for _, line := range t {
+		out+= string(line)+"\n"
+	}
+	return out
+}
+
+func (p Trails) findTrail(i, j int, char trailChar) {}
+
+func main() {
+	file, err := os.Open("example.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	totalScore := 0
+	t := make(Trails, 0)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		t = append(t, []trailChar(scanner.Text()))
+	}
+
+	fmt.Println(t)
+
+	for i := range t {
+		for j, tc := range t[i] {
+			if tc == '0' {
+				t.findTrail(i, j, one)
+			}
+		}
+	}
+
+	fmt.Println(totalScore)
+}
