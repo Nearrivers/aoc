2e8cbdef33468ffb268d51c952fbd4c2a4ac782a
Antoine <a.fourcade65@gmail.com>
2024-12-10 15:33:00 +0100

    jour 7 partie 2

--- 2024/day7/part1/part1.go
+++ 2024/day7/part1/part1.go
@@ -1,4 +1,4 @@
-package main
+package part1
 
 import (
 	"bufio"
@@ -8,6 +8,9 @@ import (
 	"strconv"
 )
 
+// I did not find the answer alone. The answer was found following
+// and understanding the code found in this repository https://github.com/pin2t/aoc2024/blob/master/07.go
+
 func addition(calibration, mesure int) int {
 	return calibration + mesure
 }
@@ -16,26 +19,14 @@ func multiplication(calibration, mesure int) int {
 	return calibration * mesure
 }
 
-func concat(calibration, mesure int) int {
-	strCal := strconv.FormatInt(int64(calibration), 10)
-	strMes := strconv.FormatInt(int64(mesure), 10)
-
-	strResult := strCal + strMes
-
-	result, _ := strconv.ParseInt(strResult, 10, 64)
-	return int(result)
-}
-
 func match(numbers []int, index, calibration int) bool {
 	if index == len(numbers) - 1 {
 		return numbers[0] == addition(calibration, numbers[index]) ||
-		numbers[0] == multiplication(calibration, numbers[index]) ||
-		numbers[0] == concat(calibration, numbers[index])
+		numbers[0] == multiplication(calibration, numbers[index])
 	}
 
 	return match(numbers, index+1, addition(calibration, numbers[index])) ||
-		match(numbers, index+1, multiplication(calibration, numbers[index])) ||
-		match(numbers, index+1, concat(calibration, numbers[index]))
+		match(numbers, index+1, multiplication(calibration, numbers[index]))
 }
 
 func main() {
