146577be0073b8df14ef31943d4602cb221ef9a7
Antoine <a.fourcade65@gmail.com>
2024-12-12 22:03:54 +0100

    jour 6 partie 2

--- 2024/day6/main.go
+++ 2024/day6/main.go
@@ -9,6 +9,10 @@ import (
 
 type Grid [][]rune
 
+type VisitedSet map[[4]int]bool
+
+var visited VisitedSet
+
 func (g Grid) String() string {
 	var out string
 	for _, row := range g {
@@ -21,89 +25,31 @@ func (g Grid) String() string {
 	return out
 }
 
-func moveLeft(rowIndex, columnIndex int, grid Grid) int {
-	count := 0
-	if grid[rowIndex][columnIndex] != 'X' {
-		grid[rowIndex][columnIndex] = 'X'
-		count++
-		fmt.Println(grid)
+func Loop(grid Grid, row, col int) bool {
+	// Indice trouvé sur la vidéo https://youtu.be/2td0PZRKkpQ?si=GAlobfr1Smy_oz8D&t=301 (avec timestamps)
+	deltaRow := -1
+	deltaCol := 0
+
+	visited = make(map[[4]int]bool)
+
+	for {
+		if row+deltaRow < 0 || row+deltaRow >= len(grid) || col+deltaCol < 0 || col+deltaCol >= len(grid[0]) {
+			return false
+		}
+		visited[[4]int{row, col, deltaRow, deltaCol}] = true
+
+		if grid[row+deltaRow][col+deltaCol] == '#' {
+			// Tourner à droite
+			deltaRow, deltaCol = deltaCol, -deltaRow
+		} else {
+			row += deltaRow
+			col += deltaCol
+		}
+
+		if _, ok := visited[[4]int{row, col, deltaRow, deltaCol}]; ok {
+			return true
+		}
 	}
-
-	if columnIndex == 0 {
-		return count
-	}
-
-	if grid[rowIndex][columnIndex-1] == '#' {
-		count += moveUp(rowIndex, columnIndex, grid)
-		return count
-	}
-
-	count += moveLeft(rowIndex, columnIndex-1, grid)
-	return count
-}
-
-func moveDown(rowIndex, columnIndex int, grid Grid) int {
-	count := 0
-	if grid[rowIndex][columnIndex] != 'X' {
-		grid[rowIndex][columnIndex] = 'X'
-		count++
-		fmt.Println(grid)
-	}
-
-	if rowIndex == len(grid)-1 {
-		return count
-	}
-
-	if grid[rowIndex+1][columnIndex] == '#' {
-		count += moveLeft(rowIndex, columnIndex, grid)
-		return count
-	}
-
-	count += moveDown(rowIndex+1, columnIndex, grid)
-	return count
-}
-
-func moveRight(rowIndex, columnIndex int, grid Grid) int {
-	count := 0
-	if grid[rowIndex][columnIndex] != 'X' {
-		grid[rowIndex][columnIndex] = 'X'
-		count++
-		fmt.Println(grid)
-	}
-
-	if columnIndex == len(grid[rowIndex])-1 {
-		return count
-	}
-
-	if grid[rowIndex][columnIndex+1] == '#' {
-		count += moveDown(rowIndex, columnIndex, grid)
-		return count
-	}
-
-	count += moveRight(rowIndex, columnIndex+1, grid)
-	return count
-}
-
-func moveUp(rowIndex, columnIndex int, grid Grid) int {
-	count := 0
-	if grid[rowIndex][columnIndex] == '.' {
-		grid[rowIndex][columnIndex] = '|'
-		count++
-		fmt.Println(grid)
-	}
-
-	if rowIndex == 0 {
-		return count
-	}
-
-	if grid[rowIndex-1][columnIndex] == '#' {
-		grid[rowIndex][columnIndex] = '+'
-		count += moveRight(rowIndex, columnIndex, grid)
-		return count
-	}
-
-	count += moveUp(rowIndex-1, columnIndex, grid)
-	return count
 }
 
 func main() {
@@ -129,7 +75,25 @@ func main() {
 		}
 	}
 
-	total := moveUp(soldierRow, soldierColumn, grid)
+	obstructionsCount := 0
+	for i := range grid {
+		for j := range grid[i] {
+			if grid[i][j] != '.' {
+				continue
+			}
 
-	fmt.Println(total)
+			// On insère un obstacle
+			grid[i][j] = '#'
+
+			// On simule une ronde du garde avec cet obstacle
+			if Loop(grid, soldierRow, soldierColumn) {
+				obstructionsCount++
+			}
+
+			// On enlève le faux obstacle
+			grid[i][j] = '.'
+		}
+	}
+
+	fmt.Println(obstructionsCount)
 }
---
+++ 2024/day6/part1/part1.go
@@ -0,0 +1,135 @@
+package part1
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Grid [][]rune
+
+func (g Grid) String() string {
+	var out string
+	for _, row := range g {
+		out += "["
+		for _, column := range row {
+			out += string(column)
+		}
+		out += "]\n"
+	}
+	return out
+}
+
+func moveLeft(rowIndex, columnIndex int, grid Grid) int {
+	count := 0
+	if grid[rowIndex][columnIndex] != 'X' {
+		grid[rowIndex][columnIndex] = 'X'
+		count++
+		fmt.Println(grid)
+	}
+
+	if columnIndex == 0 {
+		return count
+	}
+
+	if grid[rowIndex][columnIndex-1] == '#' {
+		count += moveUp(rowIndex, columnIndex, grid)
+		return count
+	}
+
+	count += moveLeft(rowIndex, columnIndex-1, grid)
+	return count
+}
+
+func moveDown(rowIndex, columnIndex int, grid Grid) int {
+	count := 0
+	if grid[rowIndex][columnIndex] != 'X' {
+		grid[rowIndex][columnIndex] = 'X'
+		count++
+		fmt.Println(grid)
+	}
+
+	if rowIndex == len(grid)-1 {
+		return count
+	}
+
+	if grid[rowIndex+1][columnIndex] == '#' {
+		count += moveLeft(rowIndex, columnIndex, grid)
+		return count
+	}
+
+	count += moveDown(rowIndex+1, columnIndex, grid)
+	return count
+}
+
+func moveRight(rowIndex, columnIndex int, grid Grid) int {
+	count := 0
+	if grid[rowIndex][columnIndex] != 'X' {
+		grid[rowIndex][columnIndex] = 'X'
+		count++
+		fmt.Println(grid)
+	}
+
+	if columnIndex == len(grid[rowIndex])-1 {
+		return count
+	}
+
+	if grid[rowIndex][columnIndex+1] == '#' {
+		count += moveDown(rowIndex, columnIndex, grid)
+		return count
+	}
+
+	count += moveRight(rowIndex, columnIndex+1, grid)
+	return count
+}
+
+func moveUp(rowIndex, columnIndex int, grid Grid) int {
+	count := 0
+	if grid[rowIndex][columnIndex] == '.' {
+		grid[rowIndex][columnIndex] = '|'
+		count++
+		fmt.Println(grid)
+	}
+
+	if rowIndex == 0 {
+		return count
+	}
+
+	if grid[rowIndex-1][columnIndex] == '#' {
+		grid[rowIndex][columnIndex] = '+'
+		count += moveRight(rowIndex, columnIndex, grid)
+		return count
+	}
+
+	count += moveUp(rowIndex-1, columnIndex, grid)
+	return count
+}
+
+func main() {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	grid := make(Grid, 0)
+
+	scanner := bufio.NewScanner(f)
+	count := -1
+	soldierRow := 0
+	soldierColumn := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		grid = append(grid, []rune(line))
+		count++
+
+		if strings.Contains(line, "^") {
+			soldierColumn = strings.Index(line, "^")
+			soldierRow = count
+		}
+	}
+
+	total := moveUp(soldierRow, soldierColumn, grid)
+
+	fmt.Println(total)
+}
